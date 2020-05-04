16be1fc1165b12ec07474ef31a7cbed243a96805
Federico Barrientos <fgbarrientos93@gmail.com>
2020-05-04 18:26:54 -0300

    update 4/5/20
    Ya puedo acceder a la camara, grabar y reproducir lo grabado
    ---
    Ahora faltaria, mostrarlo en formato gif lo grabado, subirlo a giphy, y almacenarlo en el localstorage

--- js/create-gif.js
+++ js/create-gif.js
@@ -9,105 +9,119 @@ function loadTheme(){
 
 }
 loadTheme();
+// Botenes capturados
+// const btnCapture = document.getElementById('btn-capture');
+// const btnReady = document.getElementById('btn-ready');
 
 
 
 const video = document.getElementById('video-capture');
-let btnBegin = document.getElementById('btn-comenzar');
-const stage1 = document.getElementById('stage1');
-const stage2= document.getElementById('stage2');
+
 
 function stage1ToStage2(){
     stage1.classList.add('hidden');
     stage2.classList.remove('hidden');
 }
 
+// function stage2ToStage3(){
+//     document.getElementById('gif-capture-head').innerHTML = "Capturando Tu Gifo";
+//     btnCapture.classList.add('hidden');
+//     btnReady.classList.remove('hidden');
+
+// }
+
 function testVideo(){
     stage1ToStage2();
-    navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true
-    }).then(stream => {
-        video.srcObject = stream;
-    }).catch(console.error)
+    // getMedia();
+    
 }
 
-// let constraintObj = {
-//     audio: false,
-//     video: {
-//         facingMode: "user",
-//         width:832,
-//     }
-// }
-//ACAMICA
-// function getStreamAndRecord () { 
+let constraintObj = {
+    audio: false,
+    video: true
+}
 
-//     navigator.mediaDevices.getUserMedia({
-    
-//     audio: false,
-    
-//     video: {
-    
-//     height: { max: 480 }
-    
+// if (navigator.mediaDevices === undefined) {
+//     navigator.mediaDevices = {};
+//     navigator.mediaDevices.getUserMedia = function(constraintObj) {
+//         let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+//         if (!getUserMedia) {
+//             return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+//         }
+//         return new Promise(function(resolve, reject) {
+//             getUserMedia.call(navigator, constraintObj, resolve, reject);
+//         });
 //     }
-    
+// }else{
+//     navigator.mediaDevices.enumerateDevices()
+//     .then(devices => {
+//         devices.forEach(device=>{
+//             console.log(device.kind.toUpperCase(), device.label);
+//             //, device.deviceId
+//         })
 //     })
-    
-//     .then(function(stream) {
-    
-//     video.srcObject = stream;
-    
-//     video.play();
-    
+//     .catch(err=>{
+//         console.log(err.name, err.message);
 //     })
-
-
-
-
-//VICKY
-// const videoCapture = document.getElementById('video-capture');
-// let constraints = {
-//     audio: false,
-//     video: {
-//             width: { min: 1280 },
-//             height: { min: 720 }
-// }
-// }
-// let btnCapture = document.getElementById('btn-capture');
-// //muestro el video en un chequeo antes de empezar
-// btnCapture.addEventListener("click", getAndRecordMedia)
-
-//  function getAndRecordMedia() {
-//     getMedia(constraints).then(showMedia).catch(errorGetMedia)
 // }
 
-// var media; //necesito esta varaible para parar la camara en StopRecording
-// //obtengo video
-// function getMedia(constraints) {
-//     return navigator.mediaDevices.getUserMedia(constraints).then( x=> {
-//         media = x  
-//         return media
-//   }).catch(err => {
-//           console.log("erorr tipo " + err)
-//   });
-//   }
+navigator.mediaDevices.getUserMedia(constraintObj)
+.then(function(mediaStreamObj) {
+    //connect the media stream to the first video element
+    // let video = document.querySelector('video');
+    if ("srcObject" in video) {
+        video.srcObject = mediaStreamObj;
+    } else {
+        //old version
+        video.src = window.URL.createObjectURL(mediaStreamObj);
+    }
+    
+    video.onloadedmetadata = function(ev) {
+        //show in the video element what is being captured by the webcam
+        video.play();
+    };
+    
+    //add listeners for saving video/audio
+    let btnBegin = document.getElementById('btn-comenzar');
 
-//   //muestro el video en un chequeo antes de empezar
-// function showMedia(media) {
-//     try {
-//         if (videoCapture.srcObject !== undefined) {
-//         videoCapture.srcObject = media;
-//       } else {
-//         videoCapture.src = media;
-//       }
-//           videoCapture.play()
-//     } catch (error) {
-//         alert("error: " + error)
-//     }
+    let btnCapture = document.getElementById('btn-capture');
+    let btnReady = document.getElementById('btn-ready');
+    let btnStage4 = document.getElementById('buttons-stage4');
+    let vidSave = document.getElementById('recorded-video');
+    let mediaRecorder = new MediaRecorder(mediaStreamObj);
+    let chunks = [];
 
-// }
 
-// function errorGetMedia(error) {
-//     alert(error)
-// }
+const stage1 = document.getElementById('stage1');
+const stage2= document.getElementById('stage2');
+    
+
+    btnCapture.addEventListener('click', (ev)=>{
+        mediaRecorder.start();
+        console.log(mediaRecorder.state);
+        document.getElementById('gif-capture-head').innerHTML = "Capturando Tu Gifo";
+        btnCapture.classList.add('hidden');
+        btnReady.classList.remove('hidden');
+    })
+    btnReady.addEventListener('click', (ev)=>{
+        mediaRecorder.stop();
+        console.log(mediaRecorder.state);
+        btnReady.classList.add('hidden');
+        btnStage4.classList.remove('hidden');
+        video.classList.add('hidden');
+        vidSave.classList.remove('hidden');
+     });
+    mediaRecorder.ondataavailable = function(ev) {
+        chunks.push(ev.data);
+    }
+    mediaRecorder.onstop = (ev)=>{
+        let blob = new Blob(chunks, { 'type' : 'gif;' });
+        chunks = [];
+        let videoURL = window.URL.createObjectURL(blob);
+        vidSave.src = videoURL;
+        
+    }
+})
+.catch(function(err) { 
+    console.log(err.name, err.message); 
+});
